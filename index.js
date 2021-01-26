81d22c60af7ff0e81468242e13589c10bffe9ec5
Bladi452 <cleto662@gmail.com>
2021-01-26 17:35:37 -0400

    todo borrado

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict'
+
+import { app, BrowserWindow } from 'electron'
+import devtools from './devtools'
+
+devtools()
+if(process.env.NODE_ENV === 'development'){
+}
+
+console.dir(app)
+app.on('ready', () => {
+    let win = new BrowserWindow({
+        width: 1366,
+        height: 768,
+        frame: false
+    })
+
+    win.on('closed', () => {
+        win = null,
+            app.quit()
+    })
+    
+win.loadURL(`file://${__dirname}/src/renderer/index.html`)
+
+})
