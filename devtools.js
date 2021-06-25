7dbe93a3d44bde39ea503d63272c82eba1d87b65
bladi452 <cleto662@gmail.com>
2021-06-25 17:06:37 -0400

    Base de datos terminada
    Co-authored-by: daniperezdpj <daniperezdpj@users.noreply.github.com>

--- devtools.js
+++ devtools.js
@@ -1,7 +1,6 @@
 import { enableLiveReload} from 'electron-compile'
 import electronDebug from 'electron-debug'
 
-
 module.exports = function devtools() {
     enableLiveReload();
     electronDebug({showDevTools: true})
