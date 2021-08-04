1ea7a71de678b099bd2806df23460fc3aee667f3
Bladi452 <cleto662@gmail.com>
2021-08-04 16:09:29 -0500

    chat en proceso

--- src/renderer/chat.js
+++ src/renderer/chat.js
@@ -14,6 +14,34 @@ async function renderGetProducts() {
     await ipcRenderer.invoke('getChat')
 }
 
+/*
+Mensaje del administrador
+<li class="self">
+<div class="msg">
+  <p>Puff...</p>
+  <p>Aún estoy haciendo el contexto de Góngora.</p>
+  <p>Mejor otro día</p>
+  <time>20:18</time>
+</div>
+</li>
+*/
+
+/*
+mensaje de la otra persona
+<li class="other">
+          <div class="msg">
+            <p>Hola!</p>
+            <p>Te vienes a cenar al centro?</p>
+            <time>20:17</time>
+          </div>
+        </li>
+        */
+/*
+menu
+nombre del estudiante
+<div class="name">Alex</div>              
+         */
+
 ipcRenderer.on('solicitudesNew', (event, results) => {
     let template = ""
     const list = results
@@ -25,7 +53,7 @@ ipcRenderer.on('solicitudesNew', (event, results) => {
                 <strong>Solicitud</strong>
             </center>
         </div>
-    </li>    `
+    </li>`
     });
     mylist.innerHTML = template;
 })
